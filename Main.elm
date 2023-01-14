bd74525bfc3d40004da81c00f4be3f176f77d88e
sawa <sawa.aiko@clouds-inc.jp>
2023-01-15 02:54:19 +0900

    動作がおかしかった時のやつ

--- Main.elm
+++ Main.elm
@@ -340,16 +340,13 @@ update msg model =
                         |> Maybe.Extra.unwrap (Just millis) (always Nothing)
             in
             ( model
-            , Cmd.batch
-                [ observeTouchCmd model.config
-                , Just insertTouchCmd
-                    |> Maybe.Extra.andMap touch.idm
-                    |> Maybe.Extra.andMap (Maybe.Extra.orElse (Just "") touch.data)
-                    |> Maybe.Extra.andMap zoneName
-                    |> Maybe.Extra.andMap (validateTime millis)
-                    |> Maybe.Extra.andMap model.dbh
-                    |> Maybe.withDefault Cmd.none
-                ]
+            , Just insertTouchCmd
+                |> Maybe.Extra.andMap touch.idm
+                |> Maybe.Extra.andMap (Maybe.Extra.orElse (Just "") touch.data)
+                |> Maybe.Extra.andMap zoneName
+                |> Maybe.Extra.andMap (validateTime millis)
+                |> Maybe.Extra.andMap model.dbh
+                |> Maybe.withDefault Cmd.none
             )
 
         GotSetting setting ->
@@ -380,8 +377,11 @@ update msg model =
 
         SQLDone dbh ->
             ( model
-            , Maybe.map selectAllTouchCmd model.dbh
-                |> Maybe.withDefault Cmd.none
+            , Cmd.batch
+                [ observeTouchCmd model.config
+                , Maybe.map selectAllTouchCmd model.dbh
+                    |> Maybe.withDefault Cmd.none
+                ]
             )
 
 
